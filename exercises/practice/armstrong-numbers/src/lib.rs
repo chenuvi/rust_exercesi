8598f0c1c9743b97f28788c478df4dd43618079c
chenuvi <chenuvi@outlook.com>
2024-05-08 07:03:44 +0800

    Feat:is_armstrong_number

--- exercises/practice/armstrong-numbers/src/lib.rs
+++ exercises/practice/armstrong-numbers/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    if num < 10 {
+        return true;
+    }
+
+    let digit_count = num.to_string().len() as u32;
+    let mut sum: u64 = 0;
+
+    let mut n = num as u64;
+
+    while n > 0 {
+        println!("n: {}", n);
+        sum += (n % 10).pow(digit_count);
+        n /= 10;
+    }
+    sum == num as u64
 }
