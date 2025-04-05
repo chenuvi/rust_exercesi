185738a2fb7d42664a75301b00c34646f4f03a72
chenuvi <chenuvi@outlook.com>
2025-04-05 22:45:24 +0800

    feat: clock

--- exercises/practice/clock/src/lib.rs
+++ exercises/practice/clock/src/lib.rs
@@ -2,10 +2,22 @@ pub struct Clock;
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        Clock {
+            hours: hours,
+            minutes: minutes,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+        let mut new_hours = self.hours;
+        let mut new_minutes = self.minutes + minutes;
+        while new_minutes < 0 {
+            new_minutes += 60;
+            new_hours -= 1;
+        }
+        while new_minutes >= 60 {
+            new_minutes -= 60;
+            new_hours += 1; 
+        }
     }
 }
