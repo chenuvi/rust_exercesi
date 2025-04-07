a600ff177ea06e18eb9db5336fec356b39d0fc93
kunchen16 <kunchen16@iflytek.com>
2025-04-07 15:41:38 +0800

    feat(anagram): Anagram

--- exercises/practice/anagram/src/lib.rs
+++ exercises/practice/anagram/src/lib.rs
@@ -1,5 +1,20 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a &str]) -> HashSet<&'a str> {
+    let lower = word.to_lowercase();
+    let sorted = sort(&lower);
+    possible_anagrams
+        .iter()
+        .filter(|input| {
+            let input_lower = input.to_lowercase();
+            lower != input_lower && sorted == sort(&input_lower)
+        })
+        .cloned()
+        .collect()
+}
+
+fn sort(word: &str) -> String {
+    let mut sorted: Vec<char> = word.chars().collect();
+    sorted.sort_unstable();
+    sorted.into_iter().collect()
 }
