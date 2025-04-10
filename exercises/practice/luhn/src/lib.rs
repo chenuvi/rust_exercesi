65ae5344c8fbd9b652a57ae60b049ab5f4705549
kunchen16 <kunchen16@iflytek.com>
2025-04-10 14:44:33 +0800

    feat: Luhn

--- exercises/practice/luhn/src/lib.rs
+++ exercises/practice/luhn/src/lib.rs
@@ -1,4 +1,24 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    todo!("Is the Luhn checksum for {code} valid?");
+    let trimmed = code.replace(" ", "");
+    if trimmed.len() <= 1 || !trimmed.chars().all(|char| char.is_ascii_digit()) {
+        return false;
+    }
+    let sum = trimmed
+        .chars()
+        .rev()
+        .enumerate()
+        .map(|(index, char)| {
+            let mut digit = char.to_digit(10).unwrap();
+            if index % 2 == 1 {
+                digit *= 2;
+                if digit > 9 {
+                    digit -= 9;
+                }
+            }
+            digit
+        })
+        .sum::<u32>();
+
+    sum % 10 == 0
 }
