9ed6a2a7378c76be1dbe1fbe8bfd49a74dda9026
kunchen16 <kunchen16@iflytek.com>
2025-04-30 16:56:38 +0800

    feat: series

--- exercises/practice/series/src/lib.rs
+++ exercises/practice/series/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    todo!("What are the series of length {len} in string {digits:?}")
+    match len {
+        0 => vec!["".to_string(); digits.len() + 1],
+        _ => digits
+            .chars()
+            .collect::<Vec<char>>()
+            .windows(len)
+            .map(|x| x.iter().collect::<String>())
+            .collect(),
+    }
 }
