5a5b2f3a684c5844f21899474b11bc643ac9c4aa
chenuvi <chenuvi@outlook.com>
2025-05-01 21:55:54 +0800

    feat: kinder-garden

--- exercises/practice/kindergarten-garden/src/lib.rs
+++ exercises/practice/kindergarten-garden/src/lib.rs
@@ -1,3 +1,24 @@
+const STUDENTS: [&str; 12] = [
+    "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph",
+    "Kincaid", "Larry",
+];
+
 pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-    todo!("based on the {diagram}, determine the plants the {student} is responsible for");
+    let plant_from_char = |c: char| -> &'static str {
+        match c {
+            'R' => "radishes",
+            'C' => "clover",
+            'G' => "grass",
+            'V' => "violets",
+            _ => "",
+        }
+    };
+
+    let cup_index = STUDENTS.iter().position(|&s| s== student).unwrap() * 2;
+    let lines = diagram.lines();
+    println!("lines:, {:?}", lines);
+
+    lines.flat_map(|line|  {
+        line[cup_index..=cup_index+ 1].chars().map(plant_from_char)
+    }).collect()
 }
