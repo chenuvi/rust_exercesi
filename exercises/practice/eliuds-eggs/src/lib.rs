129abebbffe27704f8702937f95f4a684ed445c2
kunchen16 <kunchen16@iflytek.com>
2025-05-06 10:18:54 +0800

    feat: egg_count

--- exercises/practice/eliuds-eggs/src/lib.rs
+++ exercises/practice/eliuds-eggs/src/lib.rs
@@ -1,3 +1,23 @@
 pub fn egg_count(display_value: u32) -> usize {
-    todo!("count the eggs in {display_value}")
+    // display_value.count_ones() as usize
+    /*
+     * 解法一： 转换字符串处理
+     */
+    // format!("{:b}", display_value)
+    //     .chars()
+    //     .filter(|c| *c == '1')
+    //     .count()
+
+    /*
+     * 解法二： 位运算
+     */
+    let mut count = 0;
+    let mut value = display_value;
+    while value > 0 {
+        // 检查最低位是否为1
+        count += value & 1;
+        // 右移一位
+        value >>= 1;
+    }
+    count as usize
 }
